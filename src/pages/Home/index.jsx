182cca9dc9e8fa44dd1cddfcde1b98f796951fc8
riltonbispo <riltonbispo1@gmail.com>
2023-05-30 20:59:54 -0300

    feat: create components card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,13 @@ function Home() {
   return ( 
     <Container fluid>
       <Row>
-        <Col xs={12} md={3}> <Card></Card> </Col>
+        <Col xs={12} md={3}> 
+          <Card 
+            image='https://images.pexels.com/photos/802195/pexels-photo-802195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            title='Titulo'
+            total={0}
+          />
+        </Col>
       </Row>
     </Container>
    );
