80b8d8b66e7f2f542f04368a745fb335688f439b
riltonbispo <riltonbispo1@gmail.com>
2023-05-30 20:59:24 -0300

    feat: create partial header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/Home"
+import Header from "./partials/Header"
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Home></Home>
     </div>
   )
---
+++ src/partials/Header/index.jsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return ( 
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Gallery</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#gallery">Galerias</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+   );
+}
+
+export default Header;
